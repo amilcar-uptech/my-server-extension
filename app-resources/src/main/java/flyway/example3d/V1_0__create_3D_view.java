d8400534b8f332870a7997d9b398d0be1ed6b606
amilcar-capsus <amilcar.gonzalez@capsus.mx>
2020-11-12 15:21:10 -0600

    Fix

--- app-resources/src/main/java/flyway/example3d/V1_0__create_3D_view.java
+++ app-resources/src/main/java/flyway/example3d/V1_0__create_3D_view.java
@@ -1,24 +1,17 @@
 package flyway.example3d;
 
-import org.flywaydb.core.api.migration.BaseJavaMigration;
-import org.flywaydb.core.api.migration.Context;
-import org.oskari.helpers.LayerHelper;
-import org.oskari.helpers.AppSetupHelper;
+import fi.nls.oskari.db.DBHandler;
+import org.flywaydb.core.api.migration.jdbc.JdbcMigration;
+
+import java.sql.Connection;
 
 /**
  * Creates a new 3D view
  */
-public class V1_0__create_3D_view extends BaseJavaMigration {
+public class V1_0__create_3D_view implements JdbcMigration {
 
-    public void migrate(Context context) throws Exception {
-         // insert layers based on json under /src/main/resources/json/layers/
-        LayerHelper.setupLayer("/json/layers/hki-3d-model.json");
-        LayerHelper.setupLayer("/json/layers/hki-3d-buildings.json");
-        LayerHelper.setupLayer("/json/layers/nasa-features.json");
-        LayerHelper.setupLayer("/json/layers/nasa-labels.json");
-        LayerHelper.setupLayer("/json/layers/nasa-water-mask.json");
-
-        // add applications based on json under /src/main/resources/json/views/
-        AppSetupHelper.create(context.getConnection(), "/json/apps/geoportal-3d.json");
+    public void migrate(Connection connection) throws Exception {
+         // run setup based on json under /src/main/resources/setup/app-example-3d.json
+        DBHandler.setupAppContent(connection, "app-example-3d.json");
     }
 }
